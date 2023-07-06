af377438ac9058ff5a4e73f42a9c69bd909c9dd5
Hasuuk3 <hasnain.khatri34@gmail.com>
2023-07-06 11:21:03 +0500

    a
    a

---
+++ public/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXPU8_UQKLg4vLHm5LOYEJfzv8kI97BYo",
+  authDomain: "hk-electonic.firebaseapp.com",
+  projectId: "hk-electonic",
+  storageBucket: "hk-electonic.appspot.com",
+  messagingSenderId: "256422911745",
+  appId: "1:256422911745:web:5259757dce5ab14da14500"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
